c331c8e888d85dc7aafa055dbffa5a8e03d415f9
Kramton <markymoo719@gmail.com>
2024-09-19 19:55:44 +1200

    Updated search bar in homepage
    Made the search bar look better

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -12,7 +12,21 @@ export function Home() {
             <div className="welcome">
                 <h1>Welcome to TaskTap!</h1>
 
-                <input type="text" className="searchBar" placeholder="Search..."/>
+                {/* <input type="text" className="searchBar" placeholder="Search..."/> */}
+                
+                <div id="cover">
+                    <form method="get" action="">
+                        <div class="tb">
+                            <div class="td"><input type="text" placeholder="Search" /></div>
+                            <div class="td" id="s-cover">
+                                <button type="submit">
+                                <div id="s-circle"></div>
+                                <span></span>
+                            </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
