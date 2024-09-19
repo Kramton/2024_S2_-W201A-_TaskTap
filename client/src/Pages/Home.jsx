714b423a6c9782db524c18aa3b362233c6be8817
Kramton <markymoo719@gmail.com>
2024-09-19 15:02:21 +1200

    Added animated background for homepage

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,17 @@
 // import { Link } from "react-router-dom"
-import "./Home.css"
+import "./Home.css";
+import bg from "../Assets/bg04.mp4";
 
 export function Home() {
     return (
         <div className="homePage">
-            <h1>Welcome to TaskTap!</h1>
+            <div className="background">
+                <video src={ bg } autoPlay loop muted></video>
+            </div>
+            
+            <div className="welcome">
+                <h1>Welcome to TaskTap!</h1>
 
-            <div className="searchBarContainer">
                 <input type="text" className="searchBar" placeholder="Search..."/>
             </div>
         </div>
