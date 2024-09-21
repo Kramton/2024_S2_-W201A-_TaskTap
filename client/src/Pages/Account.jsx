9c768b81f4c95dfb24ce718900d332edcef28474
MaxUda <yadrenijpasatig@gmail.com>
2024-09-21 15:48:39 +1200

    post-merge quick fix

--- client/src/Pages/Account.jsx
+++ client/src/Pages/Account.jsx
@@ -4,12 +4,7 @@ import { SideBar } from "../Components/SideBar";
 
 export function Account(props) {
     return (
-<<<<<<< HEAD
-        <div className="container">
-            
-=======
         <div className="accountContainer">
->>>>>>> styling-web-page
             <SideBar/>
             <div className="content">
                 <h1>Account</h1>
