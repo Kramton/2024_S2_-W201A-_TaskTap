b62d1137dd90a4eb5e1bf710a717fb837c4d279b
Sam G <sam.gudipoodi@gmail.com>
2024-10-21 19:29:57 +1300

    created the editing order functions so users can edit orders and it will also change in firebase data

--- client/src/Pages/CurrentOrder.jsx
+++ client/src/Pages/CurrentOrder.jsx
@@ -2,16 +2,17 @@ import { SideBar } from "../Components/SideBar";
 import "./CurrentOrder.css";
 import ItemList from "../Components/ItemList";
 
+
 export function CurrentOrder() {
-    return (
-        <div className="currentOrderContainer">
-            <SideBar/>
-            <div className="currentOrder">
-                <h1>Current Orders</h1>
-            </div>
-            <div className="currentOrder">
-                <ItemList/>
-            </div>    
+  return (
+    <div className="currentOrderContainer">
+      <SideBar />
+      <div className="currentOrderContent">
+        <h1>Current Orders</h1>
+        <div className="currentOrderList">
+          <ItemList />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
